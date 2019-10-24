6c3293eb03a9a51b6649159f250a4e01c7cc74a4
andrea9llo <andrea.novello93@gmail.com>
2019-10-24 11:32:46 +0200

    add lista non ordinata

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 // nuovo utente si trova
 
 // dichiaro le variabili
-var cognome,listaCognomi,posizione,elenco;
+var cognome,listaCognomi,posizione,elenco,copiaArray,elencoDue;
 
 // chiedo all'utente il cognome
 cognome = prompt("Inserisca il suo cognome");
@@ -13,15 +13,33 @@ cognome = prompt("Inserisca il suo cognome");
 // creo array con diversi cognomi
 listaCognomi = ["novello","perrucci","appezzato","cancelmo"];
 listaCognomi.push(cognome);
-listaCognomi.sort();
 
 
+// copia dell' array
+copiaArray = listaCognomi.slice();
+
+// formula per lista ordinata
+copiaArray.sort();
+
+// posizione del cognome
+posizione = parseInt(copiaArray.indexOf(cognome) + 1);
+// console.log(posizione);
+
+
+//stampo la  lista non ordinata
 for (var i = 0; i < listaCognomi.length; i++) {
   elenco = document.getElementById('lista').innerHTML;
   document.getElementById('lista').innerHTML = elenco + "<li>" + listaCognomi[i] + "  " + "</li>" ;
-  console.log(listaCognomi[i]);
+  // console.log(listaCognomi[i]);
 }
 
-posizione = parseInt(listaCognomi.indexOf(cognome) + 1);
-console.log(posizione);
+// stampo la lista ordinata
+
+for (var j = 0; j < copiaArray.length; j++) {
+  elencoDue = document.getElementById('lista2').innerHTML;
+  document.getElementById('lista2').innerHTML = elencoDue + "<li>" + copiaArray[j] + "  " + "</li>" ;
+  // console.log(copiaArray[j]);
+}
+
+// stampo la posizione del cognome inserito dall'utente
 document.getElementById('pos').innerHTML = "Il tuo cognome si trova nella posizione : " + posizione;
